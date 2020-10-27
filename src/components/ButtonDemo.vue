7a31044d7ebf75bb3cc5be5a96fdaeef7333aa28
JH <18468107890@163.com>
2020-10-27 14:59:10 +0800

    添加theme属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,15 +1,21 @@
+<!--
+ * @Date: 2020-10-20 11:29:06
+ * @LastEditTime: 2020-10-27 14:54:33
+-->
 <template>
   <div>
     <Button>你好</Button>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
   </div>
 </template>
 
 <script lang="ts">
-import Button from '../lib/Button.vue';
+import Button from "../lib/Button.vue";
 
 export default {
-  components: {Button},
-  setup() {
-  }
+  components: { Button },
+  setup() {},
 };
 </script>>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,17 +1,26 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-10-21 13:56:45
+ * @LastEditTime: 2020-10-27 14:57:21
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /sanbengzi-vue3/src/lib/Button.vue
+-->
 <template>
-  <div class="wrapper">
-    <button>
-      <slot/>
-    </button>
-  </div>
+  <button class="gulu-button" :class="`gulu-${theme}`">
+    <slot />
+  </button>
 </template>
 
 <script lang="ts">
-export default {};
+export default {
+  props: {
+    theme: {
+      type: String,
+      default: "button",
+    },
+  },
+  setup() {},
+};
 </script>
 
-<style scoped lang="scss">
-.wrapper {
-  border: 1px solid red;
-}
-</style>
