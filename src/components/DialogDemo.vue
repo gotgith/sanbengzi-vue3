99fe138e9706042771e7eb11b4526de2624f0d19
JH <18468107890@163.com>
2020-10-28 14:33:14 +0800

    创建Dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
-    Dialog
+    <Button @click="toggle">打开</Button>
+    <Dialog v-model:visible="status" :closeOnClickOverlay="false"></Dialog>
   </div>
 </template>
+
+<script lang="ts">
+import {ref} from 'vue';
+import Button from '../lib/Button.vue';
+import Dialog from '../lib/Dialog.vue';
+
+export default {
+  components: {Dialog, Button},
+  setup() {
+    const status = ref(false);
+    const toggle = () => {
+      status.value = !status.value;
+    };
+    return {status, toggle};
+  }
+};
+</script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,6 +1,6 @@
 <!--
  * @Date: 2020-10-21 13:56:45
- * @LastEditTime: 2020-10-27 17:42:09
+ * @LastEditTime: 2020-10-28 10:59:47
 -->
 <template>
   <button :disabled="disabled" class="gulu-button" :class="classes">
@@ -11,6 +11,7 @@
 
 <script lang="ts">
 import { computed } from 'vue';
+
 export default {
   props: {
     theme: {
@@ -71,63 +72,77 @@ $grey: grey;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
   transition: background 250ms;
+
   & + & {
     margin-left: 8px;
   }
+
   &:hover,
   &:focus {
     color: $blue;
     border-color: $blue;
   }
+
   &:focus {
     outline: none;
   }
+
   &::-moz-focus-inner {
     border: 0;
   }
+
   &.gulu-theme-link {
     border-color: transparent;
     box-shadow: none;
     color: $blue;
+
     &:hover,
     &:focus {
       color: lighten($blue, 10%);
     }
   }
+
   &.gulu-theme-text {
     border-color: transparent;
     box-shadow: none;
     color: inherit;
+
     &:hover,
     &:focus {
       background: darken(white, 5%);
     }
   }
+
   &.gulu-size-big {
     font-size: 24px;
     height: 48px;
     padding: 0 16px;
   }
+
   &.gulu-size-small {
     font-size: 12px;
     height: 20px;
     padding: 0 4px;
   }
+
   &.gulu-theme-button {
     &.gulu-level-main {
       background: $blue;
       color: white;
       border-color: $blue;
+
       &:hover,
       &:focus {
         background: darken($blue, 10%);
         border-color: darken($blue, 10%);
       }
     }
+
     &.gulu-level-danger {
       background: $red;
       border-color: $red;
       color: white;
+
       &:hover,
       &:focus {
         background: darken($red, 10%);
@@ -135,40 +150,49 @@ $grey: grey;
       }
     }
   }
+
   &.gulu-theme-link {
     &.gulu-level-danger {
       color: $red;
+
       &:hover,
       &:focus {
         color: darken($red, 10%);
       }
     }
   }
+
   &.gulu-theme-text {
     &.gulu-level-main {
       color: $blue;
+
       &:hover,
       &:focus {
         color: darken($blue, 10%);
       }
     }
+
     &.gulu-level-danger {
       color: $red;
+
       &:hover,
       &:focus {
         color: darken($red, 10%);
       }
     }
   }
+
   &.gulu-theme-button {
     &[disabled] {
       cursor: not-allowed;
       color: $grey;
+
       &:hover {
         border-color: $grey;
       }
     }
   }
+
   &.gulu-theme-link,
   &.gulu-theme-text {
     &[disabled] {
@@ -176,6 +200,7 @@ $grey: grey;
       color: $grey;
     }
   }
+
   > .gulu-loadingIndicator {
     width: 14px;
     height: 14px;
@@ -188,6 +213,7 @@ $grey: grey;
     animation: gulu-spin 1s infinite linear;
   }
 }
+
 @keyframes gulu-spin {
   0% {
     transform: rotate(0deg);
