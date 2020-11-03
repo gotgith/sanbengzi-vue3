3137d6b715329b266d56283e71674e05a5bb084b
JH <18468107890@163.com>
2020-11-03 11:38:28 +0800

    实现Tab切换，滚动条待实现

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,18 +1,35 @@
 <template>
-  <div>
-    <div v-for="(t,index) in titles" :key="index">{{t}}</div>
-    <!--    Tabs组件-->
-    <component v-for="(c,index) in defaults" :key="index" :is="c"></component>
+  <div class="gulu-tabs">
+    <div class="gulu-tabs-nav">
+      <div class="gulu-tabs-nav-item"
+           v-for="(t,index) in titles"
+           @click="select(t)"
+           :class="{selected:t===selected}"
+           :key="index">{{t}}
+      </div>
+      <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
+    </div>
+    <div class="gulu-tabs-content">
+      <component :is="current" :key="current.props.title"></component>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Tab from './Tab.vue';
+import {computed} from 'vue';
 
 export default {
+  props: {
+    selected: {
+      type: String
+    }
+  },
+  // setup函数只会在页面挂载组件的时候执行。
   setup(props, context) {
     console.log({...context.slots.default()});
     const defaults = context.slots.default();
+    console.log(defaults);
     defaults.forEach((item) => {
       if (item.type !== Tab) {
         throw new Error('Tabs 的子标签必须是 Tab');
@@ -21,7 +38,59 @@ export default {
     const titles = defaults.map((tags) => {
       return tags.props.title;
     });
-    return {defaults, titles};
+
+    const current = computed(() => {
+      return defaults.filter((tag) => {
+        return tag.props.title === props.selected;
+      })[0];
+    });
+
+    const select = (title: string) => {
+      context.emit('update:selected', title);
+    };
+    return {defaults, titles, current, select};
   }
 };
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.gulu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    position: relative;
+
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+
+      &:first-child {
+        margin-left: 0;
+      }
+
+      &.selected {
+        color: $blue;
+      }
+    }
+
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
+    }
+  }
+
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
